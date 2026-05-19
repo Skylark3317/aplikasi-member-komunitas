fe7321f59e93d342c004112e2756e058a3a24d69
Fikri Setiawan <fikrisetiawan2006@gmail.com>
2026-05-19 09:09:15 +0700

    menambahkan route dashboard

--- routes/auth.php
+++ routes/auth.php
@@ -56,4 +56,6 @@ Route::middleware('auth')->group(function () {
 
     Route::post('logout', [AuthenticatedSessionController::class, 'destroy'])
         ->name('logout');
+
+    Route::get('dashboard', [AuthenticatedSessionController::class, 'index'])->name('dashboard');
 });
